8193defb8e2044094a28eeb555dfdfc0ae20e4c5
ozan honamlioglu <ozan.honamlioglu@inveon.com>
2021-04-21 20:30:46 +0300

    code refactoring

--- Netflix/GlobalHelpers.swift
+++ Netflix/GlobalHelpers.swift
@@ -8,10 +8,10 @@
 import Foundation
 import SwiftUI
 
-let exampleMovie1 = Movie(id: UUID().uuidString, name: "DARK", thumbnailURL: URL(string: "https://www.dizimob1.com/resim/dizi-resim-pc/another-life.jpg")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"])
-let exampleMovie2 = Movie(id: UUID().uuidString, name: "Travelers", thumbnailURL: URL(string: "https://picsum.photos/200/302")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"])
+let exampleMovie1 = Movie(id: UUID().uuidString, name: "Travelers", thumbnailURL: URL(string: "https://images.moviesanywhere.com/6305a9e8ed76d5fa485ac16637655cf7/bcc68be4-eede-409b-a63d-e179b28d19b4.jpg")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"])
+let exampleMovie2 = Movie(id: UUID().uuidString, name: "DARK", thumbnailURL: URL(string: "https://wallpapers.moviemania.io/phone/tv/1396/b7bd15/breaking-bad-phone-wallpaper.jpg?w=1016&h=1807")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"])
 let exampleMovie3 = Movie(id: UUID().uuidString, name: "Communith", thumbnailURL: URL(string: "https://picsum.photos/200/303")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"])
-let exampleMovie4 = Movie(id: UUID().uuidString, name: "Alone", thumbnailURL: URL(string: "https://picsum.photos/200/304")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"])
+let exampleMovie4 = Movie(id: UUID().uuidString, name: "Alone", thumbnailURL: URL(string: "https://i.pinimg.com/originals/bc/d5/c9/bcd5c9519581acc60bd60a429ab0c88f.jpg")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"])
 let exampleMovie5 = Movie(id: UUID().uuidString, name: "Hanniball", thumbnailURL: URL(string: "https://picsum.photos/200/305")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"])
 let exampleMovie6 = Movie(id: UUID().uuidString, name: "After Life", thumbnailURL: URL(string: "https://picsum.photos/200/306")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"])
 
--- Netflix/View/Home/TopMoviePreview.swift
+++ Netflix/View/Home/TopMoviePreview.swift
@@ -12,7 +12,6 @@ struct TopMoviePreview: View {
     var movie: Movie
     
     @State var listAdded: Bool = false
-    @State var frame: CGSize = .zero
     
     var body: some View {
         ZStack {
@@ -22,64 +21,58 @@ struct TopMoviePreview: View {
                 .clipped()
             
             GeometryReader {
-                (geometry) in self.makeView(geometry)
+                geometry in
+                
+                VStack {
+                        Spacer()
+                        
+                        HStack {
+                            ForEach(movie.categories, id: \.self) { category in
+                                HStack {
+                                    Text(category).font(.footnote)
+                                    
+                                    if let last = movie.categories.last, last != category {
+                                        Image(systemName: "circle.fill")
+                                            .foregroundColor(.blue)
+                                            .font(.system(size: 3))
+                                    }
+                                    
+                                }
+                            }
+                        }
+
+                        HStack {
+                            Spacer()
+                            
+                            SmallVerticalButton(text: "My List", isOnImage: "checkmark", isOffImage: "plus", isOn: listAdded) {
+                                listAdded = !listAdded
+                            }
+                            
+                            Spacer()
+                            
+                            WhiteButton(text: "Play", imageName: "play.fill") {
+                                // do smt later
+                            }
+                            .frame(width: 120)
+                            
+                            Spacer()
+                            
+                            SmallVerticalButton(text: "Info", isOnImage: "info.circle", isOffImage: "info.circle", isOn: true) {
+                                // do smt later
+                            }
+                            
+                            Spacer()
+                        }
+                        .padding(.vertical, 10)
+
+                    }
+                    .background(LinearGradient.blackOpacityGradient)
+                    .padding(.top, geometry.size.height / 2)
+                
             }
             
         }
         .foregroundColor(.white)
-    }
-    
-    func makeView(_ geometry: GeometryProxy) -> some View {
-        print(geometry.size.width, geometry.size.height)
-
-        DispatchQueue.main.async { self.frame = geometry.size }
-
-        return VStack {
-                Spacer()
-                
-                HStack {
-                    ForEach(movie.categories, id: \.self) { category in
-                        HStack {
-                            Text(category).font(.footnote)
-                            
-                            if let last = movie.categories.last, last != category {
-                                Image(systemName: "circle.fill")
-                                    .foregroundColor(.blue)
-                                    .font(.system(size: 3))
-                            }
-                            
-                        }
-                    }
-                }
-
-                HStack {
-                    Spacer()
-                    
-                    SmallVerticalButton(text: "My List", isOnImage: "checkmark", isOffImage: "plus", isOn: listAdded) {
-                        listAdded = !listAdded
-                    }
-                    
-                    Spacer()
-                    
-                    WhiteButton(text: "Play", imageName: "play.fill") {
-                        // do smt later
-                    }
-                    .frame(width: 120)
-                    
-                    Spacer()
-                    
-                    SmallVerticalButton(text: "Info", isOnImage: "info.circle", isOffImage: "info.circle", isOn: true) {
-                        // do smt later
-                    }
-                    
-                    Spacer()
-                }
-                .padding(.vertical, 10)
-
-            }
-            .background(LinearGradient.blackOpacityGradient)
-            .padding(.top, geometry.size.height / 2)
-
     }
     
 }
